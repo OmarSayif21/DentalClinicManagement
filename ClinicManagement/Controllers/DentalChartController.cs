c34243bb7837e9d357d7deb36b8dab7ef9356db5
OmarSayif21 <omarsayif315@gmail.com>
2024-11-04 00:52:28 +0800

    Patient Details Fix and Dental Chart Form

---
+++ ClinicManagement/Controllers/DentalChartController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ClinicManagement.Core;
+using ClinicManagement.Core.Models;
+using ClinicManagement.Core.ViewModel;
+using Microsoft.AspNet.Identity;
+
+namespace ClinicManagement.Controllers
+{
+    public class DentalChartController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var model = new DentalChartViewModel
+            {
+                ToothStatuses = new Dictionary<string, string>()
+            };
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ClinicManagement/Core/ViewModel/DentalChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManagement.Core.ViewModel
+{
+    public class DentalChartViewModel
+    {
+        public int Id { get; set; }
+        public Dictionary<string, string> ToothStatuses { get; set; }
+    }
+}
